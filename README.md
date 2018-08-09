26ad22c23bf0d966f31f11fbd7c4cfcc0b5deba8
Hussin Saheb <Hsaheb@spartaglobal.com>
2018-08-09 22:20:52 +0100

    homework finished

---
+++ README.md
@@ -0,0 +1,22 @@
+# Sparta Calculator
+
+the aim of this work is to build a basic calculator by using the DOM object in javascript.
+
+
+### Requirements
+* The calculator will carry put basic operations
+* values to be added must be shown before the operator is given
+* result of any given operation must be displayed at the main screen at top.
+
+
+### Jasmine - test basic functions
+* this project will use jasmine for a TDD approach.
+* Rquires
+  * node.js
+  * jasmine
+
+![tests](images/tests.png)
+
+#### My approach
+I've created 4 functions that do the basic calculations: add, subtract, divide and multiply. I then used them in another function called calculation.
+calculation function takes 3 parameters: first is a number, second is an operator and third is the second number. The functions applied from the previous four depend on the operator passed as the argument. This is achieved by applying it in an switch case.
--- index.js
+++ index.js
@@ -1,1 +1,55 @@
+var number_1, number_2, operator;
 
+// retrieve the screen and save as variable
+var screen = document.getElementById('screen');
+// retrieve all the buttons
+var buttons = document.getElementsByClassName("buttonNum");
+// retriece the clear button
+var ac = document.getElementById('clear');
+
+//loop over all buttonNums and add event listener to each
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function(){
+    // if the screen has the sparta logo replace it by number clicked
+    if (screen.innerHTML == "Sparta Calculator") {
+      screen.innerHTML = this.innerHTML;
+    }else{
+      // else concat to it.
+      screen.innerHTML += this.innerHTML;
+    }
+  });
+};
+// retrieve array of operators
+var operators = document.getElementsByClassName("operator");
+// loop over the operators
+for (var i = 0; i < operators.length; i++) {
+// add addEventListener for each
+  operators[i].addEventListener("click", function(){
+    // when its pressed
+    //value at screen is number 1
+    number_1 = screen.innerHTML;
+    // operator is the button presed
+    operator = this.innerHTML;
+    // clear display from value to capture number 2
+    screen.innerHTML = "";
+  });
+}
+
+
+//retrieve the equals
+var equals = document.getElementById('equals');
+equals.addEventListener('click', function(){
+  // when equals is pressed get number 2
+  number_2 = screen.innerHTML;
+  // all parameters have been collected. use funciton and save value
+  var result = calculation(parseFloat(number_1), operator, parseFloat(number_2));
+  // display the result to screen.
+  screen.innerHTML = result;
+});
+
+
+// add click listener to clear calc
+ac.addEventListener("click", function(){
+  // clearing is basically removing all the
+  number_1,number_2, operator, screen.innerHTML = "";
+});
