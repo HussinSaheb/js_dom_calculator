811da4734d8bda3c1e1803e6a6d05f2c079548da
Hussin Saheb <Hsaheb@spartaglobal.com>
2018-08-10 09:31:11 +0100

    changed back to sparta calculator for display

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var ac = document.getElementById('clear');
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function(){
     // if the screen has the sparta logo replace it by number clicked
-    if (screen.innerHTML != "") {
+    if (screen.innerHTML == "Sparta Calculator") {
       screen.innerHTML = this.innerHTML;
     }else{
       // else concat to it.
